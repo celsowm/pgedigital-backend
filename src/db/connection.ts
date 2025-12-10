e8552fa4501ffee4e49a5d0fd77a940d8cf2ec35
celsowm <celsowm@gmail.com>
2025-12-10 18:53:21 -0300

    v

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,7 @@ export const dbConfig = {
   host: process.env.PGE_DIGITAL_HOST,
   user: process.env.PGE_DIGITAL_USER,
   password: process.env.PGE_DIGITAL_PASSWORD,
-  database: process.env.PGE_DIGITAL_DATABASE,
+  database: "PGE_DIGITAL",
   options: {
     encrypt: process.env.PGE_DIGITAL_ENCRYPT === 'true',
     trustServerCertificate: process.env.PGE_DIGITAL_TRUST_CERT === 'true'
