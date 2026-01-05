e1a873bc5e337c43ee2f3c3bd07057d303157688
celsowm <celsowm@gmail.com>
2026-01-05 17:21:38 -0300

    feat(api): refactor Especializada query handling and simplify filters

--- src/controllers/EspecializadaController.ts
+++ src/controllers/EspecializadaController.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Put, QueryStyle } from "adorn-api";
-import type { SearchWhere } from "adorn-api/metal";
+import { Controller, Get, Post, Put} from "adorn-api";
+import type { SearchWhere, ListQuery } from "adorn-api/metal";
+import { pagedOptions } from "adorn-api/metal";
 import type { Especializada } from "../db/entities/Especializada.js";
 import { withSession } from "../db/orm.js";
 import type { EspecializadaFilters } from "../repositories/EspecializadaRepository.js";
@@ -7,88 +8,24 @@ import * as EspecializadaService from "../services/EspecializadaService.js";
 import type { PaginatedResult } from "metal-orm";
 
 type EspecializadaWhere = SearchWhere<Especializada, {
-  maxDepth: 1;
   include: [
-    "responsavel_id",
-    "tipo_especializada_id",
-    "usa_pge_digital",
-    "usa_plantao_audiencia",
-    "restricao_ponto_focal",
-    "especializada_triagem",
-    "codigo_ad",
     "nome",
-    "sigla",
-    "responsavel.id",
-    "responsavel.nome",
+    "responsavel.nome"
   ];
 }>;
 
-const toEspecializadaFilters = (
-  where?: EspecializadaWhere,
-): EspecializadaFilters | undefined => {
-  if (!where) return undefined;
-
-  const filters: EspecializadaFilters = {};
-
-  if (where.responsavel_id !== undefined) {
-    filters.responsavel_id = where.responsavel_id;
-  }
-
-  if (where.tipo_especializada_id !== undefined) {
-    filters.tipo_especializada_id = where.tipo_especializada_id;
-  }
-
-  if (where.usa_pge_digital !== undefined) {
-    filters.usa_pge_digital = where.usa_pge_digital;
-  }
-
-  if (where.usa_plantao_audiencia !== undefined) {
-    filters.usa_plantao_audiencia = where.usa_plantao_audiencia;
-  }
-
-  if (where.restricao_ponto_focal !== undefined) {
-    filters.restricao_ponto_focal = where.restricao_ponto_focal;
-  }
-
-  if (where.especializada_triagem !== undefined) {
-    filters.especializada_triagem = where.especializada_triagem;
-  }
-
-  if (where.codigo_ad !== undefined) {
-    filters.codigo_ad = where.codigo_ad;
-  }
-
-  if (where.nome !== undefined) {
-    filters.nome = where.nome;
-  }
-
-  if (where.sigla !== undefined) {
-    filters.sigla = where.sigla;
-  }
-
-  if (where.responsavel?.id !== undefined && filters.responsavel_id === undefined) {
-    filters.responsavel_id = where.responsavel.id;
-  }
-
-  if (where.responsavel?.nome !== undefined) {
-    filters.responsavel_nome = where.responsavel.nome;
-  }
-
-  return Object.keys(filters).length ? filters : undefined;
-};
-
+type EspecializadaQuery =
+  Omit<ListQuery<Especializada>, "where"> & {
+    where?: EspecializadaWhere;
+  };
 
 @Controller("/especializada")
 export class EspecializadaController {
-  @Get("/")
-  @QueryStyle({ style: "deepObject" })
-  async list(
-    where?: EspecializadaWhere,
-    pagination?: { page?: number; pageSize?: number },
-  ): Promise<PaginatedResult<Especializada>> {
-    const filters = toEspecializadaFilters(where);
 
-    return withSession(session => EspecializadaService.listPaged(session, filters, pagination));
+  @Get("/")
+  async list(query?: EspecializadaQuery): Promise<PaginatedResult<Especializada>> {
+    const filters = toEspecializadaFilters(query);
+    return withSession(session => EspecializadaService.listPaged(session, filters, pagedOptions(query)));
   }
 
   @Get("/:id")
