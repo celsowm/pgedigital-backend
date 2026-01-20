9bd5926eb8c0fdca6ed40604270d254fed6c1c9b
celsowm <celsowm@gmail.com>
2026-01-20 15:20:18 -0300

    clear

--- src/controllers/HealthController.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from "adorn-api";
-
-@Controller("/health")
-export class HealthController {
-  @Get("/")
-  async status(): Promise<{ status: string }> {
-    return { status: "ok" };
-  }
-}
