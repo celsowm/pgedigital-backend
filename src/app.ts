8a9d591a016ae01e27d59dd14ce13b52fc2a72d9
celsowm <celsowm@gmail.com>
2026-01-26 10:04:48 -0300

    almost zeroing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import { createExpressApp } from "adorn-api";
-import { NotaVersaoController } from "./controllers/nota-versao/nota-versao.controller";
 import { TestController } from "./controllers/test/test.controller";
 
 export function createApp() {
   return createExpressApp({
-    controllers: [TestController, NotaVersaoController],
+    controllers: [TestController],
     openApi: {
       info: {
         title: "PGE Digital API",
